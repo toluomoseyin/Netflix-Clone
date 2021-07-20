97d8025ed427d3c54406211b935a2e0b5ecce34e
tolu omoseyin <toluomoseyin01@gmail.com>
2021-07-20 13:35:34 +0100

    finished sign up and sign in

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
-  const [error] = useState("");
+  const [error, setError] = useState("");
 
   const isInvalid = password === "" || emailAddress === "";
   const handleSignIn = (event) => {
     event.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        //push to browse page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
   };
   return (
     <>
